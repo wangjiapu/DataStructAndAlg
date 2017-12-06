9b604af3f015482d26e0ce92d7f06661630e6105
wangjiapu <2405491487@qq.com>
2017-12-06 22:03:12 +0800

    Initial commit

---
+++ src/树/BitNode.java
@@ -0,0 +1,12 @@
+package 树;
+
+public class BitNode {
+    int data;
+    BitNode leftChild;
+    BitNode rightChild;
+    public void setNode(int data,BitNode leftcl,BitNode rightcl){
+        this.data=data;
+        leftChild=leftcl;
+        rightChild=rightChild;
+    }
+}
